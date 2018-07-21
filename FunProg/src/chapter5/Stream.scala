71d56f74eeed3c7a6f41301132f0d1d34b4eba22
Mario <Mario@NoNo>
2018-07-20 22:32:33 -0500

    Stream trait definition for pattern matching

--- FunProg/src/chapter5/Stream.scala
+++ FunProg/src/chapter5/Stream.scala
@@ -1,21 +1,28 @@
 package chapter5
 
 sealed trait Stream[+A] {
-  def uncons: Option[(A, Stream[A])]
-  def isEmpty: Boolean = uncons.isEmpty
+  def toList: List[A] = {
+    def loop(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Empty => acc
+      case Cons(h, t) => loop(t(), h() :: acc)
+    }
+
+    loop(this, List())
+  }
 }
+
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
 
-  def empty[A]: Stream[A] =
-    new Stream[A] {
-      def uncons = None
-    }
+  def empty[A]: Stream[A] = Empty
 
-  def cons[A](h: => A, t: => Stream[A]): Stream[A] =
-    new Stream[A] {
-      lazy val uncons = Some((h, t))
-    }
+  def cons[A](h: => A, t: => Stream[A]): Stream[A] = {
+    lazy val head = h
+    lazy val tail = t
+    Cons(() => head, () => tail)
+  }
 
   def apply[A](as: A*): Stream[A] = {
     if (as.isEmpty) empty
